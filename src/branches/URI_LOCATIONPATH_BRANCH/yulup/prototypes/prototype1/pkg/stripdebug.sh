c50aebfb20ee4eb8a07377db698bda85da496aae
andi <andi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-09-29 18:33:22 +0000

    Deleted obsolete URI_LOCATIONPATH_BRANCH.

--- src/branches/URI_LOCATIONPATH_BRANCH/yulup/prototypes/prototype1/pkg/stripdebug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Stripping file " $1
-
-sed -e 's/\/\* DEBUG \*\/ .*//g' $1 > $1.stripped
-mv $1.stripped $1
--- src/branches/URI_LOCATIONPATH_BRANCH/yulup/prototypes/prototype1/src/xpathparser/Main.java
+++
@@ -1,9 +0,0 @@
-import antlr.*;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        ExprLexer  lexer  = new ExprLexer(System.in);
-        ExprParser parser = new ExprParser(lexer);
-        parser.locationPath();
-    }
-}
