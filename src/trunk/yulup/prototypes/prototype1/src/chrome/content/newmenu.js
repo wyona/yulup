92f36524d1cab1ac7e92e7512e04e62f3f048cd6
andi <andi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-12-12 20:48:01 +0000

    Added missing header.

--- src/trunk/yulup/prototypes/prototype1/src/chrome/content/newmenu.js
+++ src/trunk/yulup/prototypes/prototype1/src/chrome/content/newmenu.js
@@ -1,3 +1,30 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Copyright 2006 Wyona AG Zurich
+ *
+ * This file is part of Yulup.
+ *
+ * Yulup is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Yulup is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Yulup; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+
+/**
+ * @author Gregor Imboden
+ *
+ */
 
 /**
  * Converts a mime-type to a entity.
@@ -5,7 +32,7 @@
  * @param  {String} aContext  the context of the mime-type related resource (e.g new, delete)
  * @param  {String} aType     the requested entity type (e.g tooltip or label)
  * @param  {String} aMimeType the mime-type identifier
- * @return {String}           the entity name belonging to this mime-type 
+ * @return {String}           the entity name belonging to this mime-type
  */
 function mimeTypeToEntity(aContext, aType, aMimeType) {
     var entity = null;
