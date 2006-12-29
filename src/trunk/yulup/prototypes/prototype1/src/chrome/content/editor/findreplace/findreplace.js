83a26009eb912d0b76ad04067bc5c328a5b32baf
andi <andi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-12-29 15:32:57 +0000

    Added commands. Retrieve find services.

--- src/trunk/yulup/prototypes/prototype1/src/chrome/content/editor/findreplace/findreplace.js
+++ src/trunk/yulup/prototypes/prototype1/src/chrome/content/editor/findreplace/findreplace.js
@@ -29,6 +29,7 @@
 const FindReplace = {
     __editorController: null,
     __view            : null,
+    __findService     : null,
 
     onLoadListener: function () {
         /* DEBUG */ dump("Yulup:findreplace.js:FindReplace.onLoadListener() invoked\n");
@@ -41,6 +42,19 @@ const FindReplace = {
             window.close();
             return;
         }
+
+        FindReplace.__view = FindReplace.__editorController.activeView;
+
+        // get the nsIFindService
+        try {
+            FindReplace.__findService = Components.classes["@mozilla.org/find/find_service;1"].getService(Components.interfaces.nsIFindService);
+        } catch (exception) {
+            /* DEBUG */ YulupDebug.dumpExceptionToConsole("Yulup:findreplace.js:FindReplace.onLoadListener", exception);
+            Components.utils.reportError(exception);
+        }
+
+        // get the nsIWebBrowserFind
+        FindReplace.__webBrowserFind = FindReplace.__getWebBrowserFind(FindReplace.__view);
 
         // register a view change listener
         FindReplace.__editorController.addViewChangedListener(FindReplace.viewChanged);
@@ -61,6 +75,67 @@ const FindReplace = {
         /* DEBUG */ dump("Yulup:findreplace.js:FindReplace.viewChanged() invoked\n");
 
         FindReplace.__view = aView;
+    },
+
+    goUpdateFindReplaceCommand: function (aCommand) {
+        var controller = null;
+        var enabled    = null;
+
+        /* DEBUG */ dump("Yulup:findreplace.js:FindReplace.goUpdateFindReplaceCommand(\"" + aCommand + "\") invoked\n");
+
+        try {
+            controller = document.commandDispatcher.getControllerForCommand(aCommand);
+
+            enabled = false;
+
+            if (controller)
+                enabled = controller.isCommandEnabled(aCommand);
+
+            Editor.goSetFileOperationsCommandEnabled(aCommand, enabled);
+        } catch (exception) {
+            /* DEBUG */ dump("Yulup:findreplace.js:FindReplace.goUpdateFindReplaceCommand: an error occurred updating command \"" + aCommand + "\": " + exception + "\n");
+            /* DEBUG */ YulupDebug.dumpExceptionToConsole("Yulup:findreplace.js:FindReplace.goUpdateFindReplaceCommand", exception);
+        }
+    },
+
+    goDoFindReplaceCommand: function (aCommand) {
+        var controller = null;
+
+        /* DEBUG */ dump("Yulup:findreplace.js:FindReplace.goDoFindReplaceCommand(\"" + aCommand + "\") invoked\n");
+
+        try {
+            controller = document.commandDispatcher.getControllerForCommand(aCommand);
+
+            if (controller && controller.isCommandEnabled(aCommand))
+                controller.doCommand(aCommand);
+        } catch (exception) {
+            /* DEBUG */ dump("Yulup:findreplace.js:FindReplace.goDoFindReplaceCommand: an error occurred executing command \"" + aCommand + "\": " + exception + "\n");
+            /* DEBUG */ YulupDebug.dumpExceptionToConsole("Yulup:findreplace.js:FindReplace.goDoFindReplaceCommand", exception);
+        }
+    },
+
+    goSetFindReplaceCommandEnabled: function (aCmdId, aEnabled) {
+        var node = null;
+
+        /* DEBUG */ dump("Yulup:findreplace.js:FindReplace.goSetFindReplaceCommandEnabled(\"" + aCmdId + "\", \"" + aEnabled + "\") invoked\n");
+
+        node = document.getElementById(aCmdId);
+
+        if (node) {
+            if (aEnabled) {
+                node.setAttribute("disabled", "false");
+            } else {
+                node.setAttribute("disabled", "true");
+            }
+        }
+    },
+
+    __getWebBrowserFind: function (aView) {
+        if (aView.editor && aView.editor.webBrowserFind) {
+            return aView.editor.webBrowserFind;
+        } else {
+            return null;
+        }
     },
 
     __fillInitialValues: function () {
